2485b04eeb96a6ec378b0886ed0497df11df1f24
danilo.alves <danilo.alves@ezycollect.com.au>
2023-03-21 19:15:50 -0300

    First class

---
+++ aula1_cifra_cesar/app.py
@@ -0,0 +1,56 @@
+
+
+alphabet = 'abcdefghijklmnopqrstuvwyzàáãâéêóôõíúçABCDEFGHIJKLMNOPQRSTUVWYZÀÁÃÂÉÊÓÕÍÚÇ'
+    
+
+def ceasar(message, key=1, mode='e'):
+
+    def __index_rotation(position):
+        alphabet_positions = len(alphabet)
+
+        if position >= alphabet_positions:
+            return position - alphabet_positions
+        elif position <= 0:
+            return position + alphabet_positions
+        else:
+            return position
+        
+    def __index_generate():
+        converted_msg = ''
+        for c in message:
+            c_index = alphabet.find(c)
+
+            if mode == 'e':
+                new_index = c_index + key
+            elif mode == 'd':
+                new_index = c_index - key
+
+            new_position = __index_rotation(new_index)
+            
+            converted_msg = converted_msg + alphabet[new_position] 
+
+        return converted_msg
+
+    def __encrypt():
+        return __index_generate()
+    def __decrypt():
+        return __index_generate()
+
+    if mode == 'e':
+        return __encrypt()
+    elif mode == 'd':
+        return __decrypt()
+
+
+def string_validate(message):
+    for c in message:
+        c_index = alphabet.find(c)
+        if c_index == -1:
+            print(f"Character: {alphabet[c_index]} invalid!")
+            return 1
+    return message
+
+mode = input("[e] To encrypt and [d] To decrypt:\n")
+phrase = input("Text to be converted:")
+
+print(ceasar(phrase, mode=mode))
